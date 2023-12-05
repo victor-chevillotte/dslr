68acb69f8204c5aea89121c97ef168fc3efa0177
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2023-12-05 23:23:31 +0100

    Added pair plot !

---
+++ pair_plot.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+classes = [
+    "Arithmancy",
+    "Astronomy",
+    "Herbology",
+    "Defense Against the Dark Arts",
+    "Divination",
+    "Muggle Studies",
+    "Ancient Runes",
+    "History of Magic",
+    "Transfiguration",
+    "Potions",
+    "Care of Magical Creatures",
+    "Charms",
+    "Flying"
+]
+
+
+def read_csv(path):
+    try:
+        df = pd.read_csv(path, index_col="Index")
+    except FileNotFoundError:
+        print("File not found. Please set dataset_train.csv in data folder")
+        exit(1)
+    return df
+
+
+def main():
+    df = read_csv("data/dataset_train.csv")
+    df = df[classes]
+    sns.pairplot(df)
+    plt.show()
+    
+	print("From this visualization, what features are you going to use for your logistic regression?")
+
+
+if __name__ == "__main__":
+    main()
